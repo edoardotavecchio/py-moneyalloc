f69fa73d65dffd39a93eef3a7f3717f35b557f5e
Edoardo Tavecchio <edoardotavecchio@gmail.com>
2025-11-03 19:42:20 +0100

    #2

--- main.py
+++ main.py
@@ -1,39 +1,45 @@
 import tomllib
 import sys
-import math
 
 def main():
     with open("rules.toml", mode="rb") as rules:
         rules = tomllib.load(rules)
     curvalue = float(sys.argv[1])
 
-    # Beni rifugio
-    safe_haven_isin = str(rules["safe_haven"]["isin"])
-    safe_haven_amount = to_allocate(curvalue, rules["safe_haven"])
-    print("Beni rifugio (" + safe_haven_isin + "): " + str(safe_haven_amount))
+    print_allocated(rules)
 
-    # Obbligazioni statali
-    bond_isin = str(rules["bond"]["isin"])
-    bond_amount = to_allocate(curvalue, rules["bond"])
-    print("Obbligazioni statali (" + bond_isin + "): " + str(bond_amount))
+    for asset in rules['asset']:
+        if 'subtype' in asset:
+            type_amount = to_allocate(curvalue, asset)
+            for subtype in asset['subtype']:
+                isin = str(subtype['isin'])
+                amount = to_allocate(type_amount, subtype)
+                print(asset['type'] + '.' + subtype['type'] + ' (' + isin + '): ' + str(amount) + "€")
+            continue
+        isin = str(asset['isin'])
+        amount = to_allocate(curvalue, asset)
+        print(asset['type'] + ' (' + isin + '): ' + str(amount) + "€")
 
-    #Stocks
-    ##Totale
-    stock_total_amount = round((curvalue/100)*rules["stock"]["percentage"], 2)
-    print("Azionario totale: " + str(stock_total_amount))
-
-    ##A distribuzione
-    stock_distributed_isin = str(rules["stock_distributed"]["isin"])
-    stock_distributed_amount = to_allocate(stock_total_amount, rules["stock_distributed"])
-    print("Azionario a distribuzione (" + stock_distributed_isin + "): " + str(stock_distributed_amount))
-
-    ##Ad accumulo
-    stock_accumulating_isin = str(rules["stock_accumulating"]["isin"])
-    stock_accumulating_amount = to_allocate(stock_total_amount, rules["stock_accumulating"])
-    print("Azionario ad accumulo (" + stock_accumulating_isin + "): " + str(stock_accumulating_amount))
 
 def to_allocate(total, config):
-    return round(((total/100) * config["percentage"]) - config["allocated"], 2)
+    if 'allocated' in config:
+        return round(((total/100) * config["percentage"]) - config["allocated"], 2)
+    else:
+        return round(((total/100) * config["percentage"]), 2)
+
+def print_allocated(rules):
+    print("Hai indicato i seguenti valori come già allocati:")
+    for asset in rules['asset']:
+        if 'subtype' in asset:
+            for subtype in asset['subtype']:
+                print(asset['type'] + '.' + subtype['type'] + ': ' + str(subtype['allocated']) + "€")
+            continue
+        print(asset['type'] + ': ' + str(asset['allocated']) + "€")
+    print("Se vuoi sovrascriverne alcuni, modifica il file rules.toml.")
+    stop = input("Vuoi fermare l'esecuzione per modificare? (y/N)")
+    if stop == 'y':
+        sys.exit()
+    print('----------------------------------------------------------------------')
 
 main()
 
